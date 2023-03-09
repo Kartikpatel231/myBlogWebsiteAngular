dd910a51abbeefa71dd58965ec2d43111e7f5647
Kartikpatel231 <kartikpatel1950@gmail.com>
2023-03-09 12:00:00 +0530

    scrollBar&cursor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-blog-app';
+  
+  
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event) {
+    this.showScrollToTop = window.pageYOffset > 500;
+  }
+
+  showScrollToTop: boolean = false;
+
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  scrollToBottom() {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth'
+    });
+  }
 }
