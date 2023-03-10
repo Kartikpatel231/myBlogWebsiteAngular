615dd2407613fce2a4aaa43ecf70a33b36c2e50e
Kartikpatel231 <kartikpatel1950@gmail.com>
2023-03-10 15:51:03 +0530

    aboutUs & contact us

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ContactsService } from 'src/app/services/contacts';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  @ViewChild('contactForm', { static: false }) contactForm: NgForm;
 
-  constructor() { }
-
+  constructor(private contactsService:ContactsService ) { }
   ngOnInit() {
+  }
+  onSubmit(name: string, email: string, message: string) {
+    if (name.length > 0 && email.length>0 && message.length > 6) {
+      // Send the form data to your backend API using Angular's HttpClient module
+      // and display a success message to the user
+      alert('Form submitted successfully!');
+    
+    const timestamp = new Date().getTime();
+    this.contactsService.addContact(name,email,message);
+    console.log("contact saved successulyy");
+    
+    }
+    else{
+    alert("message can't be sent ");
+    }
+    this.contactForm.resetForm();
+
   }
 
 }
